d00ff36f8bbe6c1c9f6e6346e96eb6c2970728d8
Kyle Zollo-Venecek <kylezollo@gmail.com>
2022-09-07 11:03:32 -0400

    Create new R package in empty directory

--- docker/postgis/initdb-gis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tA <<-EOSQL
-  CREATE EXTENSION address_standardizer;
-EOSQL
-
-# Run all DDL create scripts
-cat /DDL/*.sql | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tA -1
