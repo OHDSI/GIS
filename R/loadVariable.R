310c27d54e16f6378e7f86cf0db1f9eb2be0ba31
Kyle Zollo <kylezollo@gmail.com>
2022-09-22 11:51:49 -0400

    Create docs for all existing functions in R package (#81)

--- R/loadVariable.R
+++ R/loadVariable.R
@@ -1,3 +1,33 @@
+#' Loads a variable from a registered source into PostGIS as attr_X
+#'
+#' @param conn (DatabaseConnectorJdbcConnection) A database connection object created with \code{DatabaseConnector::connect} function
+#' @param connectionDetails (list) An object of class connectionDetails as created by the createConnectionDetails function
+#' @param variableSourceId (integer) The identifier for the variable that is registered in the PostGIS variable_source table
+#'
+#' @return A table (attr_X) in PostGIS
+#'
+#' @examples
+#' \dontrun{
+#'
+#' variableSourceId <- 157
+#'
+#' variableTable <- DatabaseConnector::dbGetQuery(conn, paste0("SELECT * FROM backbone.variable_source WHERE variable_source_id = ", variableSourceId))
+#'
+#' attrIndex <- DatabaseConnector::dbGetQuery(conn, paste0("SELECT * FROM backbone.attr_index WHERE data_source_id = ", variableTable$data_source_uuid,";"))
+#'
+#' tableExists <- DatabaseConnector::existsTable(conn,
+#'                                               attrIndex$table_schema,
+#'                                               paste0("attr_", attrIndex$table_name))
+#'
+#' if (!tableExists) {
+#'   message("Loading attr table dependency")
+#'   loadVariable(conn = conn, connectionDetails = connectionDetails, variableSourceId)
+#' }
+#' }
+#'
+#' @export
+#'
+
 loadVariable <- function(conn, connectionDetails, variableSourceId){
 
   # get variable
@@ -51,7 +81,14 @@ loadVariable <- function(conn, connectionDetails, variableSourceId){
 
 }
 
-
+#' Import a well-formatted attribute table into an empty instance of attr_X in PostGIS
+#'
+#' @param conn (DatabaseConnectorJdbcConnection) A database connection object created with \code{DatabaseConnector::connect} function
+#' @param attribute (data.frame) A well-formatted attribute. Created by appending staging data table to an attr_template
+#' @param attrIndex (data.frame) A full record (entire row) from the backbone.attr_index table corresponding to the registered attribute of interest
+#'
+#' @return A table (attr_X) in PostGIS
+#'
 
 importAttrTable <- function(conn, attribute, attrIndex){
 
@@ -70,6 +107,26 @@ importAttrTable <- function(conn, attribute, attrIndex){
 
 }
 
+#' Create an empty instance of the attr_template in a given schema in PostGIS
+#'
+#' @param conn (DatabaseConnectorJdbcConnection) A database connection object created with \code{DatabaseConnector::connect} function
+#' @param schema (character) The name of the schema in PostGIS where the empty attr_X should be created
+#' @param name (character) The suffix for the table name to be created; will be appended to "attr_"
+#'
+#' @return An empty table (attr_X) in PostGIS
+#'
+#' @examples
+#' \dontrun{
+#'
+#' variableSourceId <- 157
+#'
+#' variableTable <- DatabaseConnector::dbGetQuery(conn, paste0("SELECT * FROM backbone.variable_source WHERE variable_source_id = ", variableSourceId))
+#'
+#' attrIndex <- DatabaseConnector::dbGetQuery(conn, paste0("SELECT * FROM backbone.attr_index WHERE data_source_id = ", variableTable$data_source_uuid,";"))
+#'
+#' createAttrInstanceTable(conn, schema = attrIndex$table_schema, name = attrIndex$table_name)
+#' }
+#'
 
 createAttrInstanceTable <- function(conn, schema, name) {
   DatabaseConnector::dbExecute(conn, paste0("CREATE SCHEMA IF NOT EXISTS ", schema, ";"))
@@ -95,7 +152,21 @@ createAttrInstanceTable <- function(conn, schema, name) {
 }
 
 
+#' Create a mapping table between identifiers and values for a given geom_X
+#'
+#' @param conn (DatabaseConnectorJdbcConnection) A database connection object created with \code{DatabaseConnector::connect} function
+#' @param geomIndex (integer) Identifier of a record in the backbone.geom_index table. Usually sourced from the \code{attr_of_geom_index_id} entry of an attr_index record
+#'
+#' @return (data.frame) A mapping table between geom_record_id for a given geom_X and that record's source value
+#'
+#' @examples
+#' \dontrun{
+#' geomIdMap <- getGeomIdMap(conn = conn, geomIndex = 1)
+#' }
+#'
+
 getGeomIdMap <- function(conn, geomIndex){
+  #TODO change the argument geomIndex to geomIndexId, which is what it is
   geomIndex <- DatabaseConnector::dbGetQuery(conn, paste0("SELECT * FROM backbone.geom_index WHERE geom_index_id = ", geomIndex,";"))
 
 sqlQuery <- paste0(
@@ -108,8 +179,17 @@ sqlQuery <- paste0(
   DatabaseConnector::dbGetQuery(conn, sqlQuery)
 }
 
-assignGeomIdToAttr <- function(conn, stagedResult, geomIndex){
+#' Join a column of geom_X record identifiers to a staged attr_X table
+#'
+#' @param conn (DatabaseConnectorJdbcConnection) A database connection object created with \code{DatabaseConnector::connect} function
+#' @param stagedResult (data.frame) A table standardized in the attr_template or geom_template mold
+#' @param geomIndex (integer) Identifier of a record in the backbone.geom_index table. Usually sourced from the \code{attr_of_geom_index_id} entry of an attr_index record
+#'
+#' @return (data.frame) An updated \code{stagedResult} table with \code{geom_record_id}s corresponding to a geom_X table appended
+#'
 
+assignGeomIdToAttr <- function(conn, stagedResult, geomIndex){
+  #TODO change the argument geomIndex to geomIndexId, which is what it is
   geomIdMap <- getGeomIdMap(conn, geomIndex)
 
   tmp <- merge(x = stagedResult, y= geomIdMap, by.x = "geom_join_column", by.y = "geom_source_value")
