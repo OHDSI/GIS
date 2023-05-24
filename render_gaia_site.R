6f79e60e255f8e1bbd2bcbb37220aa01ae7d523f
Kyle Zollo-Venecek <kylezollo@gmail.com>
2023-05-24 15:59:56 -0400

    Update website with geocoding

--- render_gaia_site.R
+++ render_gaia_site.R
@@ -1,5 +1,6 @@
 #' A function to render RMD site and embed R package docs
 render_gaia_site <- function() {
+  devtools::document('packages/gaiaCore')
   rmarkdown::render_site('rmd')
   pkgdown::build_site('packages/gaiaCore')
 }
