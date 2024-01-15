9bdf7b2e27976ac52fcd84759f0816820866a3c4
Kyle Zollo <kylezollo@gmail.com>
2024-01-15 11:30:25 -0500

    Fill out use case process documentation (#309)
    * Fill out use case process documentation'
    
    * Fill out use case process documentation'
    
    * Render use case docs
    
    * Make geom_type_source_value optional

--- inst/ddl/001/gaiadb_001_primary_keys.sql
+++ inst/ddl/001/gaiadb_001_primary_keys.sql
@@ -1,7 +1,7 @@
 -- Primary Key Constraints for gaiaDB version 001
-ALTER TABLE backbone.data_source ADD CONSTRAINT xpk_data_source PRIMARY KEY (data_source_uuid);
-ALTER TABLE backbone.variable_source ADD CONSTRAINT xpk_variable_source PRIMARY KEY (variable_source_id);
-ALTER TABLE backbone.attr_index ADD CONSTRAINT xpk_attr_index PRIMARY KEY (attr_index_id);
-ALTER TABLE backbone.geom_index ADD CONSTRAINT xpk_geom_index PRIMARY KEY (geom_index_id);
-ALTER TABLE backbone.attr_template ADD CONSTRAINT xpk_attr_template PRIMARY KEY (attr_record_id);
-ALTER TABLE backbone.geom_template ADD CONSTRAINT xpk_geom_template PRIMARY KEY (geom_record_id);
+ALTER TABLE backbone.data_source  ADD CONSTRAINT xpk_data_source PRIMARY KEY (data_source_uuid);
+ALTER TABLE backbone.variable_source  ADD CONSTRAINT xpk_variable_source PRIMARY KEY (variable_source_id);
+ALTER TABLE backbone.attr_index  ADD CONSTRAINT xpk_attr_index PRIMARY KEY (attr_index_id);
+ALTER TABLE backbone.geom_index  ADD CONSTRAINT xpk_geom_index PRIMARY KEY (geom_index_id);
+ALTER TABLE backbone.attr_template  ADD CONSTRAINT xpk_attr_template PRIMARY KEY (attr_record_id);
+ALTER TABLE backbone.geom_template  ADD CONSTRAINT xpk_geom_template PRIMARY KEY (geom_record_id);
